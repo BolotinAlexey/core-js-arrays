8b6fccb66a6f37a80222f5fa8ac0b3d0f7a39ba5
Oleksii Bolotin <samkiral73@gmail.com>
2024-01-21 14:55:33 +0100

    feat: generateOdds is done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -362,8 +362,10 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len * 2).fill(0).reduce((a, b, i) => {
+    return i % 2 ? [...a, i] : a;
+  }, []);
 }
 
 /**
