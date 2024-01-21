712f3046705c9cc1276f1feec74c9bc18820d00b
Oleksii Bolotin <samkiral73@gmail.com>
2024-01-21 14:45:51 +0100

    feat: createChunks is done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -336,8 +336,18 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  let chunk = [];
+  const res = arr.reduce((a, b, i) => {
+    if ((i + 1) % chunkSize) {
+      chunk.push(b);
+      return a;
+    }
+    a.push([...chunk, b]);
+    chunk = [];
+    return a;
+  }, []);
+  return chunk.length ? [...res, chunk] : res;
 }
 
 /**
